c0ec5e884a17ca3c030f6094eb6995c07d749ba4
rijalkwn <95176315+rijalkwn@users.noreply.github.com>
2023-05-27 20:29:01 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,43 +20,30 @@ class UserSeeder extends Seeder
             'nik' => '1111111111111111',
             'nama' => 'Admin Satu',
             'email' => 'admin1@gmail.com',
-            'password' => Hash::make('123'),
+            'password' => Hash::make('12345678'),
             'role' => 'admin',
         ]);
         DB::table('users')->insert([
             'nik' => '2222222222222222',
             'nama' => 'Admin Dua',
             'email' => 'admin2@gmail.com',
-            'password' => Hash::make('123'),
+            'password' => Hash::make('12345678'),
             'role' => 'admin',
         ]);
         DB::table('users')->insert([
-            'nik' => '123',
+            'nik' => '1231111111111111',
             'nama' => 'Rijal kurniawan',
             'email' => 'rijal@gmail.com',
-            'password' => Hash::make('123'),
+            'password' => Hash::make('12345678'),
             'role' => 'user',
         ]);
         DB::table('users')->insert([
-            'nik' => '124',
-            'nama' => 'Lutfiani',
-            'email' => 'lutfiani@gmail.com',
-            'password' => Hash::make('123'),
+            'nik' => '1241111111111111',
+            'nama' => 'kurniawan',
+            'email' => 'kurniawan@gmail.com',
+            'password' => Hash::make('12345678'),
             'role' => 'user',
         ]);
-        DB::table('users')->insert([
-            'nik' => '125',
-            'nama' => 'Paijo',
-            'email' => 'paijo@gmail.com',
-            'password' => Hash::make('123'),
-            'role' => 'user',
-        ]);
-        DB::table('users')->insert([
-            'nik' => '126',
-            'nama' => 'King',
-            'email' => 'king@gmail.com',
-            'password' => Hash::make('123'),
-            'role' => 'user',
-        ]);
+
     }
 }
